69aa14773b674eafe1fd86c835c5cb4ba31fedb0
Skyler <marc@macbookpro.local>
2013-08-10 16:50:01 -0600

    // test comment on skyler's purple computer at 4:49 on 8/10/13

--- Matchismo/MatchismoViewController.m
+++ Matchismo/MatchismoViewController.m
@@ -23,6 +23,7 @@
 // test comment on skyler's computer at 4:30 on 8/3/13
 // test comment on david's computer at 4:38 on 8/3/13
 // test comment on skyler's purple computer at 4:45 on 8/10/13
+// test comment on skyler's purple computer at 4:49 on 8/10/13
 
 - (void)viewDidLoad
 {
