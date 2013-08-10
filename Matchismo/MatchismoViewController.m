d720897b2beecdca0e687c23a4e1edcc2520ed2b
leserman <david@leserman.com>
2013-08-10 17:39:34 -0600

    test comment on david's computer at 5:39 on 8/10/13

--- Matchismo/MatchismoViewController.m
+++ Matchismo/MatchismoViewController.m
@@ -25,6 +25,7 @@
 // test comment on skyler's purple computer at 4:45 on 8/10/13
 // test comment on skyler's purple computer at 4:49 on 8/10/13
 // test comment on skyler's purple computer at 5:30 on 8/10/13
+// test comment on david's computer at 5:39 on 8/10/13
 
 - (void)viewDidLoad
 {
