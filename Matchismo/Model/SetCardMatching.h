06e9e2d8c1607f0691f244739de46a8a020c8581
Skyler Arnold <skyarnold1@me.com>
2013-12-05 15:17:48 -0700

    psudo code for setCardMatchingGame

---
+++ Matchismo/Model/SetCardMatching.h
@@ -0,0 +1,13 @@
+//
+//  SetCardMatching.h
+//  Matchismo
+//
+//  Created by Skyler Arnold on 12/5/13.
+//  Copyright (c) 2013 Skyler Arnold. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface SetCardMatching : CardMatchingGame
+
+@end
---
+++ Matchismo/Model/SetCardMatching.m
@@ -0,0 +1,36 @@
+//
+//  SetCardMatching.m
+//  Matchismo
+//
+//  Created by Skyler Arnold on 12/5/13.
+//  Copyright (c) 2013 Skyler Arnold. All rights reserved.
+//
+
+#import "SetCardMatching.h"
+
+@implementation SetCardMatching
+
+/*
+init:
+ init with cardcount,
+ return self
+ 
+flipCardAtIndex
+{
+ make a card which is the same as the passed in card
+ flip the card
+ if (card is playable and is face up) {
+    do any scoring (not sure how that works in set...
+    find and get the value of other cards which are being compaired {
+        figure out if it's a match
+            if it is, then make the cards unplayable
+            do scoring
+            
+        else
+            if no match, the card goes back to face down
+            do scoring
+    }
+}
+*/
+
+@end
