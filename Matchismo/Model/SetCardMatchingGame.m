f739edd91bf14af63fa37e7537c9fd81f8e03493
leserman <david@leserman.com>
2013-12-22 16:36:45 -0700

    developed code in SetCardMatchingGame isMatch()

--- Matchismo/Model/SetCardMatchingGame.m
+++ Matchismo/Model/SetCardMatchingGame.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Skyler Arnold. All rights reserved.
 //
 
+#import "SetCard.h"
 #import "SetCardMatchingGame.h"
 
 @interface SetCardMatchingGame()
@@ -20,10 +21,35 @@
 - (id)initWithCardCount:(NSUInteger)cardCount usingDeck:(Deck *)deck
 {
     self = [super initWithCardCount:cardCount usingDeck:deck];
-    
     return (SetCardMatchingGame *) self;
 }
 
+// Note:
+//  removeObjectAtIndex():
+//  Removes the object at index.
+//  To fill the gap, all elements beyond index are moved by subtracting 1 from their index.
+
+- (BOOL)isMatch
+{
+    // They all have the same shading, or they have three different shadings.
+    // They all have the same color, or they have three different colors.
+    
+    BOOL match = NO;
+
+    SetCard *card1 = self.currentlySelectedCards[0];
+    SetCard *card2 = self.currentlySelectedCards[1];
+    SetCard *card3 = self.currentlySelectedCards[2];
+    // They all have the same number, or they have three different numbers.
+    if (card1.count == card2.count && card2.count == card3.count) match = YES;
+    if (card1.count != card2.count && card2.count != card3.count && card1.count != card3.count) match = YES;
+    // They all have the same symbol, or they have three different symbols.
+    if (card1.shape == card2.shape && card2.shape == card3.shape) match = YES;
+    if (card1.shape != card2.shape && card2.shape != card3.shape && card1.shape != card3.shape) match = YES;
+    if (card1.color == card2.color && card2.color == card3.color) match = YES;
+    if (card1.color != card2.color && card2.color != card3.color && card1.color != card3.color) match = YES;
+    if (card1.fill == card2.fill && card2.fill == card3.fill) match = YES;
+    if (card1.fill != card2.fill && card2.fill != card3.fill && card1.fill != card3.fill) match = YES;
+}
 
 - (void)selectCardAtIndex:(NSUInteger)index
 {
@@ -40,6 +66,9 @@
         }
     }
     
+    // Maintain currentlySelectedCards array
+    // We decide that for no match, the last card to be selected remains selected and the other cards are set deselected in the UI and removed from currentlySelectedCards.
+    // We decide that the user can deselect a card.
     
     
     
